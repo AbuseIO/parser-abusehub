e9c88d5147148e1daa2aaca3e68822aa301769fd
Bart Vrancken <bart@abuse.io>
2015-07-14 22:38:37 +0200

    fix array

--- config/abusehub.php
+++ config/abusehub.php
@@ -1,67 +1,53 @@
 <?php
 
-return
-    [
-        'parser' =>
-        [
-            'name'          => 'AbuseHub',
-            'enabled'       => true,
-            'report_file'   => '/^abusehubcsv\-.*\.csv/i',
-            'sender_map'    =>
-                [
-                '/reports@reports.abusehub.nl/',
-                ],
+return [
+    'parser' => [
+        'name'          => 'AbuseHub',
+        'enabled'       => true,
+        'report_file'   => '/^abusehubcsv\-.*\.csv/i',
+        'sender_map'    => [
+            '/reports@reports.abusehub.nl/',
+        ],
+        'body_map'      => [
+            //
+        ],
+    ],
 
-            'body_map'      =>
-                [
-                    //
-                ],
+    'feeds' => [
+        'Shadowserver Sinkhole HTTP Drone' => [
+            'class'     => 'Shadowserver Sinkhole HTTP Drone',
+            'type'      => 'Abuse',
+            'enabled'   => true,
+            'fields'    => [
+                //
+            ],
         ],
 
-        'feeds' =>
-        [
-
-            'Shadowserver Sinkhole HTTP Drone' =>
-                [
-                'class'     => 'Shadowserver Sinkhole HTTP Drone',
-                'fields'    =>
-                    [
-                        //
-                    ],
-                'type'      => 'Abuse',
-                'enabled'   => true,
-                ],
-
-            'Shadowserver Botnet Drone' =>
-                [
-                'class'     => 'Shadowserver Botnet Drone',
-                'fields'    =>
-                    [
-                        //
-                    ],
-                'type'      => 'Abuse',
-                'enabled'   => true,
-                ],
-
-            'Shadowserver Compromised Website' => [
-                'class'     => 'Shadowserver Compromised Website',
-                'fields'    =>
-                    [
-                        //
-                    ],
-                'type'      => 'Abuse',
-                'enabled'   => true,
+        'Shadowserver Botnet Drone' => [
+            'class'     => 'Shadowserver Botnet Drone',
+            'type'      => 'Abuse',
+            'enabled'   => true,
+            'fields'    => [
+                //
             ],
-
-            'Shadowserver Microsoft Sinkhole HTTP Drone' => [
-                'class'     => 'Shadowserver Microsoft Sinkhole HTTP Drone',
-                'fields'    =>
-                    [
-                        //
-                    ],
-                'type'      => 'Abuse',
-                'enabled'   => true,
-            ],
-
         ],
-    ];
+
+        'Shadowserver Compromised Website' => [
+            'class'     => 'Shadowserver Compromised Website',
+            'type'      => 'Abuse',
+            'enabled'   => true,
+            'fields'    => [
+                //
+            ],
+        ],
+
+        'Shadowserver Microsoft Sinkhole HTTP Drone' => [
+            'class'     => 'Shadowserver Microsoft Sinkhole HTTP Drone',
+            'type'      => 'Abuse',
+            'enabled'   => true,
+            'fields'    => [
+                //
+            ],
+        ],
+    ],
+];

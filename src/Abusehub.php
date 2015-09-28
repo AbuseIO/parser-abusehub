8a0a6c9007f41b51df90d8dbc20312f0ede3b77c
Mark Stunnenberg <mark.stunnenberg@tele2.com>
2015-09-28 12:23:01 +0200

    Used filesystem stuff from Parser

--- src/Abusehub.php
+++ src/Abusehub.php
@@ -3,13 +3,9 @@
 namespace AbuseIO\Parsers;
 
 use Ddeboer\DataImport\Reader;
-use Ddeboer\DataImport\Writer;
-use Ddeboer\DataImport\Filter;
-use Illuminate\Filesystem\Filesystem;
 use SplFileObject;
-use Uuid;
-use Log;
 use ReflectionClass;
+use Log;
 
 class Abusehub extends Parser
 {
@@ -34,82 +30,65 @@ class Abusehub extends Parser
     {
         // Generalize the local config based on the parser class name.
         $reflect = new ReflectionClass($this);
-        $configBase = 'parsers.' . $reflect->getShortName();
+        $this->configBase = 'parsers.' . $reflect->getShortName();
 
         Log::info(
             get_class($this). ': Received message from: '.
             $this->parsedMail->getHeader('from') . " with subject: '" .
             $this->parsedMail->getHeader('subject') . "' arrived at parser: " .
-            config("{$configBase}.parser.name")
+            config("{$this->configBase}.parser.name")
         );
 
+        // Define array where all events are going to be saved in.
         $events = [ ];
 
         foreach ($this->parsedMail->getAttachments() as $attachment) {
             // Only use the Abusehub formatted csv, skip all others
-            if (!preg_match(config("{$configBase}.parser.report_file"), $attachment->filename)) {
+            if (!preg_match(config("{$this->configBase}.parser.report_file"), $attachment->filename)) {
                 continue;
             }
 
-            $tempUUID = Uuid::generate(4);
-            $tempPath = "/tmp/${tempUUID}/";
-            $filesystem = new Filesystem;
+            // Create temporary working environment for the parser ($this->tempPath, $this->fs)
+            $this->createWorkingDir();
+            file_put_contents($this->tempPath . $attachment->filename, $attachment->getContent());
 
-            if (!$filesystem->makeDirectory($tempPath)) {
-                return $this->failed("Unable to create directory ${tempPath}");
-            }
-
-            file_put_contents($tempPath . $attachment->filename, $attachment->getContent());
-
-            $csvReader = new Reader\CsvReader(new SplFileObject($tempPath . $attachment->filename));
+            $csvReader = new Reader\CsvReader(new SplFileObject($this->tempPath . $attachment->filename));
             $csvReader->setHeaderRowNumber(0);
 
-            foreach ($csvReader as $row) {
-                if (empty($row['report_type'])) {
+            foreach ($csvReader as $report) {
+                if (empty($report['report_type'])) {
                     return $this->failed(
                         "Unabled to detect feed because of required field report_type is missing"
                     );
                 }
 
-                $feedName = $row['report_type'];
+                $feedName = $report['report_type'];
 
-                if (empty(config("{$configBase}.feeds.{$feedName}"))) {
-                    $filesystem->deleteDirectory($tempPath);
+                // If feed is known and enabled, validate data and save report
+                if ($this->isKnownFeed($feedName) && $this->isEnabledFeed($feedName)) {
+                    // Sanity checks (skip if required fields are unset)
+                    if ($this->hasRequiredFields($feedName, $report) === true) {
+                        $events[] = [
+                            'source'        => config("{$this->configBase}.parser.name"),
+                            'ip'            => $report['src_ip'],
+                            'domain'        => false,
+                            'uri'           => false,
+                            'class'         => config("{$this->configBase}.feeds.{$feedName}.class"),
+                            'type'          => config("{$this->configBase}.feeds.{$feedName}.type"),
+                            'timestamp'     => strtotime($report['event_date'] .' '. $report['event_time']),
+                            'information'   => json_encode($report),
+                        ];
+                    } else {
+                        return $this->failed(
+                            "Required field {$this->requiredField} is missing in the report or config is incorrect."
+                        );
+                    }
+                } else {
                     return $this->failed(
-                        "Detected feed '{$feedName}' is unknown."
+                        "Detected feed '{$feedName}' is unknown or disabled."
                     );
                 }
-
-                if (config("{$configBase}.feeds.{$feedName}.enabled") !== true) {
-                    continue;
-                }
-
-                $columns = array_filter(config("{$configBase}.feeds.{$feedName}.fields"));
-                if (count($columns) > 0) {
-                    foreach ($columns as $column) {
-                        if (!isset($row[$column])) {
-                            return $this->failed(
-                                "Required field ${column} is missing in the report or config is incorrect."
-                            );
-                        }
-                    }
-                }
-
-                $event = [
-                    'source'        => config("{$configBase}.parser.name"),
-                    'ip'            => $row['src_ip'],
-                    'domain'        => false,
-                    'uri'           => false,
-                    'class'         => config("{$configBase}.feeds.{$feedName}.class"),
-                    'type'          => config("{$configBase}.feeds.{$feedName}.type"),
-                    'timestamp'     => strtotime($row['event_date'] .' '. $row['event_time']),
-                    'information'   => json_encode($row),
-                ];
-
-                $events[] = $event;
             }
-
-            $filesystem->deleteDirectory($tempPath);
         }
 
         return $this->success($events);
